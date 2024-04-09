5154ba1a7133e6ebd17897ce98676e1d77a7e8ff
Bharath9747 <bharath.m@accolitedigital.com>
2024-04-09 12:33:50 +0530

    Initialized Project

---
+++ ticketmaster-theatre-backend/src/main/java/com/example/app/TicketMasterTheatreApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketMasterTheatreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketMasterTheatreApplication.class, args);
+	}
+
+}
